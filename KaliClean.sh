e228531cfa2c662af8caef71ed9d416224764123
Adam Ryan <51381387+lazryn@users.noreply.github.com>
2019-12-08 23:13:37 -0500

    Update KaliClean.sh

--- KaliClean.sh
+++ KaliClean.sh
@@ -31,27 +31,25 @@ sudo apt-get purge --auto-remove -y wfuzz
 sudo apt-get purge --auto-remove -y sqlmap
 sudo apt-get purge --auto-remove -y hydra
 sudo apt-get purge --auto-remove -y curl
-#pth-net
-#pth-openchangeclient
-#pth-rpcclient
 sudo apt-get purge --auto-remove -y smbclient
 sudo apt-get purge --auto-remove -y sqsh
 sudo apt-get purge --auto-remove -y winexe
-#pth-wmic
-#pth-wmis
 sudo apt-get purge --auto-remove -y cewl
 sudo apt-get purge --auto-remove -y crunch
 sudo apt-get purge --auto-remove -y john
 sudo apt-get purge --auto-remove -y medusa
 sudo apt-get purge --auto-remove -y ncrack
 sudo apt-get purge --auto-remove -y mfoc
-#nfc-list
+sudo apt-get purge --auto-remove -y libnfc-bin
+sudo apt-get purge --auto-remove -y libnfc5:armhf
 #mfc-mfclassic
 sudo apt-get purge --auto-remove -y aircrack-ng
 sudo apt-get purge --auto-remove -y nasm
 sudo apt-get purge --auto-remove -y metasploit-framework
 #searchsploit
 sudo apt-get purge --auto-remove -y wireshark
-sudo apt-get purge --auto-remove -y proxy chains
+sudo apt-get purge --auto-remove -y wireshark-common
+
+sudo apt-get purge --auto-remove -y proxychains
 
 
